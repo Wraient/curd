c2de1598692c48f69a3ca69b974937f1adece085
Wraient <wraient.911@gmail.com>
2024-08-25 22:56:54 +0530

    Pretty much done.

--- curd.py
+++ curd.py
@@ -20,11 +20,17 @@ user_id = os.environ.get('ANILIST_USER_ID')
 # print(access_token)
 # print(user_id)
 
+
 def run_script(script):
     os.system(f"./scripts/{script}.sh")
 
+
+
+
 print("downloading user data")
 anilist_user_data = get_user_data(access_token, user_id)
+with open("response.json", "w") as response:
+    response.write(str(anilist_user_data))
 try:
     if anilist_user_data['data'] == None:
         print("Cannot process user data.")
@@ -33,30 +39,40 @@ try:
 except:
     pass
 
-with open("scripts/tmp/query", "w") as file:
-    file.write(input("Enter the anime you want:\n"))
+# Searching for anime
 
-# os.system("./scripts/anime_list.sh"
+# with open("scripts/tmp/query", "w") as file:
+#     file.write(input("Enter the anime you want:\n"))
 
-run_script("anime_list")
+# os.system("./scripts/anime_list.sh")
 
-with open('response.json', 'r') as file:
-    data = json.load(file)
+
+# with open('response.json', 'r') as file:
+    # data = json.load(file)
 
 # print(data)
 # print(type(data['data']))
 
-# anime_dict = extract_anime_info(anilist_user_data)[0]
+
+
+anime_dict = extract_anime_info(anilist_user_data)[0]
 # print(anime_dict)
-anime_dict = load_anime_data("scripts/tmp/anime_list")
-print(anime_dict)
+# anime_dict = load_anime_data("scripts/tmp/anime_list")
+# print(anime_dict)
 # print(f"ANIME DICT = {anime_dict}")
+
 select_anime(anime_dict)
 
 with open("scripts/tmp/anime", "r") as anime:
     anime_name = anime.read()
 
-# Remove everything from the last '(' including the parentheses
+with open("scripts/tmp/query", "w") as query_file:
+    query_file.write(anime_name)
+
+run_script("anime_list")
+
+anime_dict = load_anime_data("scripts/tmp/anime_list")
+
 cleaned_text = re.sub(r'\(.*$', '', anime_name).strip()
 
 try:
@@ -65,10 +81,24 @@ try:
     anime_id = result['id']
     progress = int(result['progress'])
     title = result['english_title']
+    total_episodes = result['episodes']
 except:
     print("fucked up")
     anime_id = 98460
     progress = 0
+
+try:
+    print("Trying to automate")
+    with open("scripts/tmp/id", "w") as id_file:
+        id_file.write(anime_dict[anime_name+f" ({total_episodes} episodes)"])
+    with open("scripts/tmp/anime", "w") as anime_name_file:
+        anime_name_file.write(anime_name)
+except:
+    print("Cannot automate")
+    select_anime(anime_dict)
+
+# Remove everything from the last '(' including the parentheses
+
 
 # os.system("./scripts/episode_list.sh")
 run_script("episode_list")
@@ -79,35 +109,44 @@ run_script("episode_list")
 #     last_episode = temp[-1]
 # ep_no = input(f"Enter episode number: (number of episodes: {last_episode})\n")
 
-with open("scripts/tmp/ep_no", "w") as ep_no_file:
-    # ep_no.write("1")
-    # ep_no_file.write(ep_no)
-    with open("scripts/tmp/episode_list") as ep_list:
-        temp = ep_list.read()
-        temp = temp.split()
-        last_episode = int(temp[-1])
+while True:
+    with open("scripts/tmp/ep_no", "w") as ep_no_file:
+        with open("scripts/tmp/episode_list") as ep_list:
+            temp = ep_list.read()
+            temp = temp.split()
+            last_episode = int(temp[-1])
+        
+        if progress == last_episode:
+            user_input = input("Do you want to start this anime from beginning?")
+            if user_input.lower() == "yes" or user_input.lower() == "y" or user_input.lower() == "":
+                progress = 0
+            else:
+                progress = last_episode - 1
+        
+        # print(progress)
+        ep_no_file.write(str(progress+1))
+
+    os.system("./scripts/episode_url.sh")
+
+    # Print the result
+    if anime_id:
+        print(f"Anime ID: {anime_id}")
+    else:
+        print("Anime not found.")
+
+    # with open("scripts/tmp/link", 'r') as links:
+    #     link = links.read()
+
+    links = load_links("scripts/tmp/links")
     
-    if progress == last_episode:
-        user_input = input("Do you want to start this anime from beginning?")
-        if user_input.lower() == "yes" or user_input.lower() == "y" or user_input.lower() == "":
-            progress = 0
+    try:
+        start_video(links[0][1])
+    except ConnectionRefusedError:
+        print("Player Closed")
+        to_continue_or_not_to_continue = input("Continue? (y/n)\n")
+        if to_continue_or_not_to_continue.lower() == "yes" or to_continue_or_not_to_continue.lower() == "y" or to_continue_or_not_to_continue == "":
+            pass
         else:
-            progress = last_episode - 1
-    
-    print(progress)
-    ep_no_file.write(str(progress+1))
-
-os.system("./scripts/episode_url.sh")
-
-# Print the result
-if anime_id:
-    print(f"Anime ID: {anime_id}")
-else:
-    print("Anime not found.")
-
-# with open("scripts/tmp/link", 'r') as links:
-#     link = links.read()
-
-links = load_links("scripts/tmp/links")
-
-start_video(links[0][1])
+            break
+    # except:
+    #     pass
