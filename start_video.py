643e6f72f65aa989fef1a32752ffdb5acb380dc4
Wraient <wraient.911@gmail.com>
2024-08-26 02:27:44 +0530

    another process mpv

--- start_video.py
+++ start_video.py
@@ -1,45 +1,14 @@
 import os
 import json
 import socket
-import random
+import subprocess
 
-def start_video(link):
-    salt = random.randint(0,500)
-    os.system(f"mpv --input-ipc-server=/tmp/mpvsocket{salt} {link}")
+def start_video(link, salt):
+    # print(f"SALT IS {salt}")
+    subprocess.Popen(['alacritty', '-e', 'bash', '-c', f"mpv --input-ipc-server=/tmp/mpvsocket{salt} {link}"])
+    # os.system()
     # Path to the MPV socket
-    socket_path = f'/tmp/mpvsocket{salt}'
-
+    # socket_path = f'/tmp/mpvsocket{salt}'
     # Connect to the MPV IPC socket
-    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-    client.connect(socket_path)
-
-
-def send_command(command):
-    """Send a command to MPV and return the response."""
-    client.send((json.dumps(command) + '\n').encode('utf-8'))
-    response = client.recv(1024)
-    return json.loads(response.decode('utf-8'))
-
-def get_time_pos():
-    """Get the current playback time position."""
-    command = {"command": ["get_property", "time-pos"]}
-    response = send_command(command)
-    return response.get('data', 0)
-
-def pause():
-    """Pause the playback."""
-    command = {"command": ["set_property", "pause", True]}
-    send_command(command)
-
-def play():
-    """Resume the playback."""
-    command = {"command": ["set_property", "pause", False]}
-    send_command(command)
-
-def stop():
-    """Stop the playback."""
-    command = {"command": ["quit"]}
-    send_command(command)
-
-# def get_data():
-
+    # client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    # client.connect(socket_path)
