7161e8bbe11de037e435a85f79cd1aa0e16ad961
Wraient <wraient.911@gmail.com>
2024-08-25 17:19:03 +0530

    start video function

--- start_video.py
+++ start_video.py
@@ -1,14 +1,18 @@
 import os
 import json
 import socket
+import random
 
-os.system("mpv --input-ipc-server=/tmp/mpvsocket 'https://video.wixstatic.com/video/53f59c_bddcaac9d0b940c8b0238a429a47177e/1080p/mp4/file.mp4'")
-# Path to the MPV socket
-socket_path = '/tmp/mpvsocket'
+def start_video(link):
+    salt = random.randint(0,500)
+    os.system(f"mpv --input-ipc-server=/tmp/mpvsocket{salt} {link}")
+    # Path to the MPV socket
+    socket_path = f'/tmp/mpvsocket{salt}'
 
-# Connect to the MPV IPC socket
-client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-client.connect(socket_path)
+    # Connect to the MPV IPC socket
+    client = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    client.connect(socket_path)
+
 
 def send_command(command):
     """Send a command to MPV and return the response."""
@@ -37,14 +41,5 @@ def stop():
     command = {"command": ["quit"]}
     send_command(command)
 
-try:
-    while True:
-        time_pos = get_time_pos()
-        print(f"Current Time: {time_pos:.2f} seconds")
-        # Add any control commands here, for example, to pause or resume
-        # pause() or play() or stop()
-        time.sleep(1)
-except KeyboardInterrupt:
-    stop()
-finally:
-    client.close()
+# def get_data():
+
