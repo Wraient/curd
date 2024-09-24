a41f6592de7fa43a910c295315dc731510ab77eb
Wraient <wraient.911@gmail.com>
2024-09-25 03:09:33 +0530

    Absolute one file Reasons for this change: 1. One file makes it easy for the user (maybe i will create a developement branch where there would be various diffrent files for ease of development) 2. Reasons from last commit 3. Monarchy is the best 4. Look at those line of code :yum_yum: 5.Yes, i really do like pain

--- curd.py
+++ curd.py
@@ -639,7 +639,7 @@ def get_contents_of(tmp_file_name):
         return temp_file.read()
 
 def run_script(script: str):
-    os.system(f"{current_dir}/scripts/{script}.sh")
+    os.system(f"/tmp/curd/{script}.sh")
 
 def write_to_tmp(tmp_filename:str, content:str):
     try:
@@ -784,7 +784,191 @@ def load_config() -> dict:
 
     return config_dict
 
+# ----------------------------------------------- Create Bash Script ----------------------------------------
+
+def create_anime_list():
+    script_path = "/tmp/curd/anime_list.sh"
+    with open(script_path, "w") as _:
+        _.write(r"""search_anime() {
+    search_gql="query(        \$search: SearchInput        \$limit: Int        \$page: Int        \$translationType: VaildTranslationTypeEnumType        \$countryOrigin: VaildCountryOriginEnumType    ) {    shows(        search: \$search        limit: \$limit        page: \$page        translationType: \$translationType        countryOrigin: \$countryOrigin    ) {        edges {            _id name availableEpisodes __typename       }    }}"
+
+    curl -e "$allanime_refr" -s -G "${allanime_api}/api" --data-urlencode "variables={\"search\":{\"allowAdult\":false,\"allowUnknown\":false,\"query\":\"$1\"},\"limit\":40,\"page\":1,\"translationType\":\"$mode\",\"countryOrigin\":\"ALL\"}" --data-urlencode "query=$search_gql" -A "$agent" | sed 's|Show|\
+|g' | sed -nE "s|.*_id\":\"([^\"]*)\",\"name\":\"([^\"]*)\".*${mode}\":([1-9][^,]*).*|\1	\2 (\3 episodes)NEWLINEFROMHERE|p"
+}
+
+agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0"
+allanime_refr="https://allanime.to"
+allanime_base="allanime.day"
+allanime_api="https://api.${allanime_base}"
+mode="${ANI_CLI_MODE:-sub}"
+download_dir="${ANI_CLI_DOWNLOAD_DIR:-.}"
+quality="${ANI_CLI_QUALITY:-best}"
+case "$(uname -a)" in
+    *Darwin*) player_function="${ANI_CLI_PLAYER:-iina}" ;;            # mac OS
+    *ndroid*) player_function="${ANI_CLI_PLAYER:-android_mpv}" ;;     # Android OS (termux)
+    *steamdeck*) player_function="${ANI_CLI_PLAYER:-flatpak_mpv}" ;;  # steamdeck OS
+    *MINGW* | *WSL2*) player_function="${ANI_CLI_PLAYER:-mpv.exe}" ;; # Windows OS
+    *ish*) player_function="${ANI_CLI_PLAYER:-iSH}" ;;                # iOS (iSH)
+    *) player_function="${ANI_CLI_PLAYER:-mpv}" ;;                    # Linux OS
+esac
+
+# Read the entire file content into a variable
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+query=$(cat /tmp/curd/curd_query)  # This is a shorthand for cat "$filename"
+
+# query="one piece"
+anime_list=$(search_anime "$query")
+
+echo $anime_list > /tmp/curd/curd_anime_list
+""")
+    os.chmod(script_path, 0o755)
+
+def create_episode_list():
+    script_path = "/tmp/curd/episode_list.sh"
+    with open(script_path, "w") as _:
+        _.write(r"""episodes_list() {
+    episodes_list_gql="query (\$showId: String!) {    show(        _id: \$showId    ) {        _id availableEpisodesDetail    }}"
+
+    curl -e "$allanime_refr" -s -G "${allanime_api}/api" --data-urlencode "variables={\"showId\":\"$*\"}" --data-urlencode "query=$episodes_list_gql" -A "$agent" | sed -nE "s|.*$mode\":\[([0-9.\",]*)\].*|\1|p" | sed 's|,|\
+|g; s|"||g' | sort -n -k 1
+}
+
+# setup
+agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0"
+allanime_refr="https://allanime.to"
+allanime_base="allanime.day"
+allanime_api="https://api.${allanime_base}"
+mode="${ANI_CLI_MODE:-sub}"
+download_dir="${ANI_CLI_DOWNLOAD_DIR:-.}"
+quality="${ANI_CLI_QUALITY:-best}"
+case "$(uname -a)" in
+    *Darwin*) player_function="${ANI_CLI_PLAYER:-iina}" ;;            # mac OS
+    *ndroid*) player_function="${ANI_CLI_PLAYER:-android_mpv}" ;;     # Android OS (termux)
+    *steamdeck*) player_function="${ANI_CLI_PLAYER:-flatpak_mpv}" ;;  # steamdeck OS
+    *MINGW* | *WSL2*) player_function="${ANI_CLI_PLAYER:-mpv.exe}" ;; # Windows OS
+    *ish*) player_function="${ANI_CLI_PLAYER:-iSH}" ;;                # iOS (iSH)
+    *) player_function="${ANI_CLI_PLAYER:-mpv}" ;;                    # Linux OS
+esac
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+id=$(cat /tmp/curd/curd_id)
+echo $(episodes_list "$id") > /tmp/curd/curd_episode_list
+""")
+    os.chmod(script_path, 0o755)
+
+def create_episode_url():
+    script_path = "/tmp/curd/episode_url.sh"
+    with open(script_path, "w") as _:
+        _.write(r"""# # extract the video links from response of embed urls, extract mp4 links form m3u8 lists
+get_links() {
+    episode_link="$(curl -e "$allanime_refr" -s "https://${allanime_base}$*" -A "$agent" | sed 's|},{|\
+|g' | sed -nE 's|.*link":"([^"]*)".*"resolutionStr":"([^"]*)".*|\2 >\1|p;s|.*hls","url":"([^"]*)".*"hardsub_lang":"en-US".*|\1|p')"
+
+    case "$episode_link" in
+        *repackager.wixmp.com*)
+            extract_link=$(printf "%s" "$episode_link" | cut -d'>' -f2 | sed 's|repackager.wixmp.com/||g;s|\.urlset.*||g')
+            for j in $(printf "%s" "$episode_link" | sed -nE 's|.*/,([^/]*),/mp4.*|\1|p' | sed 's|,|\
+|g'); do
+                printf "%s >%s\n" "$j" "$extract_link" | sed "s|,[^/]*|${j}|g"
+            done | sort -nr
+            ;;
+        *vipanicdn* | *anifastcdn*)
+            if printf "%s" "$episode_link" | head -1 | grep -q "original.m3u"; then
+                printf "%s" "$episode_link"
+            else
+                extract_link=$(printf "%s" "$episode_link" | head -1 | cut -d'>' -f2)
+                relative_link=$(printf "%s" "$extract_link" | sed 's|[^/]*$||')
+                curl -e "$allanime_refr" -s "$extract_link" -A "$agent" | sed 's|^#.*x||g; s|,.*|p|g; /^#/d; $!N; s|\
+| >|' | sed "s|>|>${relative_link}|g" | sort -nr
+            fi
+            ;;
+        *) [ -n "$episode_link" ] && printf "%s\n" "$episode_link" ;;
+    esac
+    # echo $episode_link >> links
+    # [ -z "$ANI_CLI_NON_INTERACTIVE" ] && printf "\033[1;32m%s\033[0m Links Fetched\n" "$provider_name" 1>&2
+    [ -z "$ANI_CLI_NON_INTERACTIVE" ] 1>&2
+}
+
+# # innitialises provider_name and provider_id. First argument is the provider name, 2nd is the regex that matches that provider's link
+provider_init() {
+    provider_name=$1
+    provider_id=$(printf "%s" "$resp" | sed -n "$2" | head -1 | cut -d':' -f2 | sed 's/../&\
+/g' | sed 's/^01$/9/g;s/^08$/0/g;s/^05$/=/g;s/^0a$/2/g;s/^0b$/3/g;s/^0c$/4/g;s/^07$/?/g;s/^00$/8/g;s/^5c$/d/g;s/^0f$/7/g;s/^5e$/f/g;s/^17$/\//g;s/^54$/l/g;s/^09$/1/g;s/^48$/p/g;s/^4f$/w/g;s/^0e$/6/g;s/^5b$/c/g;s/^5d$/e/g;s/^0d$/5/g;s/^53$/k/g;s/^1e$/\&/g;s/^5a$/b/g;s/^59$/a/g;s/^4a$/r/g;s/^4c$/t/g;s/^4e$/v/g;s/^57$/o/g;s/^51$/i/g;' | tr -d '\n' | sed "s/\/clock/\/clock\.json/")
+}
+
+# # +s links based on given provider
+generate_link() {
+    case $1 in
+        1) provider_init "wixmp" "/Default :/p" ;;     # wixmp(default)(m3u8)(multi) -> (mp4)(multi)
+        2) provider_init "dropbox" "/Sak :/p" ;;       # dropbox(mp4)(single)
+        3) provider_init "wetransfer" "/Kir :/p" ;;    # wetransfer(mp4)(single)
+        4) provider_init "sharepoint" "/S-mp4 :/p" ;;  # sharepoint(mp4)(single)
+        *) provider_init "gogoanime" "/Luf-mp4 :/p" ;; # gogoanime(m3u8)(multi)
+    esac
+    [ -n "$provider_id" ] && get_links "$provider_id"
+    # echo $links >> links
+}
+
+select_quality() {
+    case "$1" in
+        best) result=$(printf "%s" "$links" | head -n1) ;;
+        worst) result=$(printf "%s" "$links" | grep -E '^[0-9]{3,4}' | tail -n1) ;;
+        *) result=$(printf "%s" "$links" | grep -m 1 "$1") ;;
+    esac
+    [ -z "$result" ] && printf "Specified quality not found, defaulting to best\n" 1>&2 && result=$(printf "%s" "$links" | head -n1)
+    printf "%s" "$result" | cut -d'>' -f2
+}
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# # gets embed urls, collects direct links into provider files, selects one with desired quality into $episode
+get_episode_url() {
+    # get the embed urls of the selected episode
+    episode_embed_gql="query (\$showId: String!, \$translationType: VaildTranslationTypeEnumType!, \$episodeString: String!) {    episode(        showId: \$showId        translationType: \$translationType        episodeString: \$episodeString    ) {        episodeString sourceUrls    }}"
+
+    resp=$(curl -e "$allanime_refr" -s -G "${allanime_api}/api" --data-urlencode "variables={\"showId\":\"$id\",\"translationType\":\"$mode\",\"episodeString\":\"$ep_no\"}" --data-urlencode "query=$episode_embed_gql" -A "$agent" | tr '{}' '\n' | sed 's|\\u002F|\/|g;s|\\||g' | sed -nE 's|.*sourceUrl":"--([^"]*)".*sourceName":"([^"]*)".*|\2 :\1|p')
+    # generate links into sequential files
+    cache_dir="$(mktemp -d)"
+    providers="1 2 3 4 5"
+    for provider in $providers; do
+        generate_link "$provider" >"$cache_dir"/"$provider" &
+    done
+    wait
+    # echo $links > links
+    # select the link with matching quality
+    links=$(cat "$cache_dir"/* | sed 's|^Mp4-||g;/http/!d' | sort -g -r -s)
+    rm -r "$cache_dir"
+    episode=$(select_quality "$quality")
+    echo $links > /tmp/curd/curd_links
+    [ -z "$episode" ] && die "Episode not released!"
+}
+
+# setup
+agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0"
+allanime_refr="https://allanime.to"
+allanime_base="allanime.day"
+allanime_api="https://api.${allanime_base}"
+sub_or_dub=$(cat /tmp/curd/curd_mode)
+mode="${ANI_CLI_MODE:-$sub_or_dub}"
+download_dir="${ANI_CLI_DOWNLOAD_DIR:-.}"
+quality="${ANI_CLI_QUALITY:-best}"
+case "$(uname -a)" in
+    *Darwin*) player_function="${ANI_CLI_PLAYER:-iina}" ;;            # mac OS
+    *ndroid*) player_function="${ANI_CLI_PLAYER:-android_mpv}" ;;     # Android OS (termux)
+    *steamdeck*) player_function="${ANI_CLI_PLAYER:-flatpak_mpv}" ;;  # steamdeck OS
+    *MINGW* | *WSL2*) player_function="${ANI_CLI_PLAYER:-mpv.exe}" ;; # Windows OS
+    *ish*) player_function="${ANI_CLI_PLAYER:-iSH}" ;;                # iOS (iSH)
+    *) player_function="${ANI_CLI_PLAYER:-mpv}" ;;                    # Linux OS
+esac
+id=$(cat /tmp/curd/curd_id)
+ep_no=$(cat /tmp/curd/curd_ep_no)
+get_episode_url 
+""")
+    os.chmod(script_path, 0o755)
+
 # ----------------------------------------------- Start of script ----------------------------------------
+
+create_anime_list()
+create_episode_list()
+create_episode_url()
 
 current_dir = Path(__file__).parent
 # current_dir = os.path.dirname(os.path.abspath(__file__))
