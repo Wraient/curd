04162f30f598c962cd8a48755a1b6ef2e6e01e48
Wraient <wraient.911@gmail.com>
2024-12-18 18:31:55 +0530

    q should not quit in selection

--- internal/selection_menu.go
+++ internal/selection_menu.go
@@ -51,7 +51,7 @@ func (m *Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			// Return quit selection option instead of quitting the program
 			m.filteredKeys[m.selected] = SelectionOption{"quit", "-1"}
 			return m, tea.Quit // Properly exit the program
