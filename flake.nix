4b05a1fdf7e26caac51d2e7bd48b3ff10a2b3644
Jonvemo <rettouseisama@gmail.com>
2025-01-19 12:13:57 -0500

    chore(flake): flake.nix improvements
    Better Structure
    
     Used let to define helpers and reduce duplication
     Support for multiple systems (x86_64-linux and aarch64-linux)
    
    Best Practices
    
     Used rec to self-reference attributes within derivation
     Used version interpolation in URL: v${version}
     Used with pkgs; to reduce repetition of pkgs.
     Direct reference to buildInputs in makeBinPath
    
    New Features
    
     Added support for nix run via apps
     Defined a default package
     Specified mainProgram in meta
    
    Improved Meta
    
     Added mainProgram field
    
    Modularity
    
     Helper functions make it easier to add more systems or future modifications
     Better code organization for maintenance

--- flake.nix
+++ flake.nix
@@ -1,32 +1,65 @@
 {
-  description = "Curd Flake";
+  description = "A CLI anime streaming tool with AniList integration";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux = let
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      
+      # Helper function to generate attrsets for multiple systems
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      
+      # Helper function to get pkgs for each system
+      pkgsFor = system: import nixpkgs { inherit system; };
+      
     in {
-      curd = pkgs.stdenv.mkDerivation {
-        pname = "curd";
-        version = "1.0.5";
-        src = pkgs.fetchurl {
-          url = "https://github.com/Wraient/curd/releases/download/v1.0.5/curd";
-          sha256 = "1rkdy6p13i4213hhx7s08ix75in0xjpfvpxcsbjrpdlr14kc044s";
+      packages = forAllSystems (system: let 
+        pkgs = pkgsFor system;
+      in {
+        default = self.packages.${system}.curd;  # Default Package
+        
+        curd = pkgs.stdenv.mkDerivation rec {
+          pname = "curd";
+          version = "1.0.5";
+          
+          src = pkgs.fetchurl {
+            url = "https://github.com/Wraient/curd/releases/download/v${version}/curd";
+            sha256 = "1rkdy6p13i4213hhx7s08ix75in0xjpfvpxcsbjrpdlr14kc044s";
+          };
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          
+          buildInputs = with pkgs; [ 
+            mpv 
+            ueberzugpp
+            rofi
+          ];
+
+          phases = ["installPhase"];
+          
+          installPhase = ''
+            install -Dm755 $src $out/bin/curd
+            wrapProgram $out/bin/curd \
+              --prefix PATH : ${pkgs.lib.makeBinPath buildInputs}
+          '';
+
+          meta = with pkgs.lib; {
+            description = "Watch anime in CLI with AniList Tracking, Discord RPC, and automatic intro/outro skipping";
+            homepage = "https://github.com/Wraient/curd";
+            license = licenses.gpl3;
+            platforms = platforms.linux;
+            mainProgram = "curd";
+          };
         };
-        nativeBuildInputs = [ pkgs.makeWrapper ];
-        buildInputs = [ pkgs.mpv ];
-        phases = ["installPhase"];
-        installPhase = ''
-          install -Dm755 $src $out/bin/curd
-          wrapProgram $out/bin/curd \
-            --prefix PATH : ${pkgs.lib.makeBinPath [ pkgs.mpv ]}
-        '';
-        meta = with pkgs.lib; {
-          description = "Watch anime in CLI with AniList Tracking, Discord RPC, Intro/Outro/Filler/Recap Skipping, etc";
-          homepage = "https://github.com/Wraient/curd";
-          license = licenses.gpl3;
-          platforms = platforms.linux;
+      });
+
+      # Add apps to allow use with `nix run`
+      apps = forAllSystems (system: {
+        default = {
+          type = "app";
+          program = "${self.packages.${system}.curd}/bin/curd";
         };
-      };
+      });
     };
-  };
 }
