16d268468456af1c0e67aeb3a6409899b7d7b3e6
Wraient <wraient.911@gmail.com>
2025-09-22 21:20:34 +0530

    release: v1.1.6

--- cmd/curd/main.go
+++ cmd/curd/main.go
@@ -86,7 +86,7 @@ func main() {
 	if *versionFlag {
 		internal.RestoreScreen()
 		if version == "" {
-			version = "1.1.5"
+			version = "1.1.6"
 		}
 		fmt.Printf("Curd version: %s\n", version)
 		os.Exit(0)
